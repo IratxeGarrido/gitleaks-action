58e6d509614cae049e45c7b3ab6eba462d5e91b7
Ryan <90624129+rheien@users.noreply.github.com>
2021-11-29 13:56:33 +0100

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+INPUT_CONFIG_PATH="$1"
+
+#set own rules for gitleaks
+CONFIG="--config ./gitleaks-action/rules.toml"
+
+echo running gitleaks "$(gitleaks --version) with the following command👇"
+
+#DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
+
+if [ "$GITHUB_EVENT_NAME" = "push" ]
+then
+  echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
+  gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commit=$GITHUB_SHA $CONFIG
+elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
+then 
+  git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
+  echo gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
+  gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
+fi
+
+if [ $? -eq 1 ]
+then
+  GITLEAKS_RESULT=$(echo -e "\e[31m🛑 STOP! Gitleaks encountered leaks")
+  echo "$GITLEAKS_RESULT"
+  echo "::set-output name=exitcode::$GITLEAKS_RESULT"
+  echo "----------------------------------"
+  echo "$CAPTURE_OUTPUT"
+  echo "::set-output name=result::$CAPTURE_OUTPUT"
+  echo "----------------------------------"
+  echo -e $DONATE_MSG
+  exit 1
+else
+  GITLEAKS_RESULT=$(echo -e "\e[32m✅ SUCCESS! Your code is good to go!")
+  echo "$GITLEAKS_RESULT"
+  echo "::set-output name=exitcode::$GITLEAKS_RESULT"
+  echo "------------------------------------"
+  echo -e $DONATE_MSG
+fi
